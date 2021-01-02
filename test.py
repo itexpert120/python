207bae2e4250be98ab98b2f005ea2ef19abb0bf9
zeeshan <itexpert120@gmail.com>
2021-01-02 12:31:00 +0500

    update

--- test.py
+++ test.py
@@ -1,24 +1,5 @@
-def pass_gen():
-    import random
-    import string
-
-    length = int(input("How long do you want the password to be? => ") or 0)
-
-    punctuation = "!@#$%^&*()"
-    symbols = string.ascii_letters + string.digits + punctuation
-    random = random.choices(symbols, k=length)
-
-    if length == 0:
-        print("Wrong Input. Program Terminated")
-        exit()
-    else:
-        print("Your New Password is:", ''.join(random))
-        print("")
-
-
 def main():
     pass_gen()
-
     while True:
         imput = input("Wanna try again? (y/N) => ") or "n"
         if "y" in imput.lower():
@@ -26,6 +7,19 @@ def main():
         else:
             break
 
+def pass_gen():
+    import random
+    import string
+    length = int(input("How long do you want the password to be? => ") or 0)
+    punctuation = "!@#$%^&*()"
+    symbols = string.ascii_letters + string.digits + punctuation
+    random = random.choices(symbols, k=length)
+    if length == 0:
+        print("Wrong Input. Program Terminated")
+        exit()
+    else:
+        print("Your New Password is:", ''.join(random))
+        print("")
 
 main()
 print("Thanks for using this Script")
